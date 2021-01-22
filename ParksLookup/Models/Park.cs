9143e81780988c340a7adea94cbcc5b02672c4d1
Constantine <faustlarsen@gmail.com>
2021-01-22 09:52:06 -0700

    add parks Model and DbContext

---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ParksLookup.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+    public string Description { get; set; }
+  }
+} 
