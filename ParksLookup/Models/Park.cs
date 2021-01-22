fd218121322cb7063691cc45eefa60ac10d42288
Constantine <faustlarsen@gmail.com>
2021-01-22 11:31:05 -0700

    add Type of parks,update model,Db and migrations

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -7,6 +7,7 @@ namespace ParksLookup.Models
     public int ParkId { get; set; }
     public string Name { get; set; }
     public string State { get; set; }
+    public string Type { get; set; }
     public string Description { get; set; }
   }
 } 
