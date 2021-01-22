597f149c38e2d1e0736aa60f7ea49903386e44ac
Constantine <faustlarsen@gmail.com>
2021-01-22 12:40:36 -0700

    add model validations

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -5,9 +5,17 @@ namespace ParksLookup.Models
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(20)]
     public string State { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Type { get; set; }
+    [Required]
+    [StringLength(1000)]
     public string Description { get; set; }
   }
 } 
