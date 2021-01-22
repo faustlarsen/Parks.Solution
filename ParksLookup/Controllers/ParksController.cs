06c3844b644b623c41d0cd7a57623e6e7fd1ca24
Constantine <faustlarsen@gmail.com>
2021-01-22 12:26:28 -0700

    add parksController with CRUD

---
+++ ParksLookup/Controllers/ParksController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksLookup.Models;
+
+namespace ParksLookup.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private ParksLookupContext _db;
+        public ParksController(ParksLookupContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> Get(string name, string state, string type)
+        {
+            var query = _db.Parks.AsQueryable();
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+            if (state != null)
+            {
+                query = query.Where(entry => entry.State == state);
+            }
+            if (type != null)
+            {
+                query = query.Where(entry => entry.Type == type);
+            }
+            List<Park> parksList = query.ToList();
+            return parksList;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Park parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -16,23 +16,14 @@ namespace ParksLookup.Models
       builder.Entity<Park>()
         .HasData(
             new Park { ParkId = 1, Name = "Saguaro", State = "Arizona", Type = "National Park", Description = "Split into the separate Rincon Mountain and Tucson Mountain districts, this park is evidence that the dry Sonoran Desert is still home to a great variety of life spanning six biotic communities." },
-
             new Park { ParkId = 2, Name = "Grand Canyon", State = "Arizona", Type = "National Park", Description = "The Grand Canyon, carved by the mighty Colorado River. Millions of years of erosion have exposed the multicolored layers of the Colorado Plateau in mesas and canyon walls." },
-
             new Park { ParkId = 3, Name = "Petrified Forest", State = "Arizona", Type = "National Park", Description = "This portion of the Chinle Formation has a large concentration of 225-million-year-old petrified wood. Dinosaur fossils and over 350 Native American sites are protected in this park" },
-
             new Park { ParkId = 4, Name = "Glacier Bay", State = "Alaska", Type = "National Park", Description = "Glacier Bay contains tidewater glaciers, mountains, fjords, and a temperate rainforest, and is home to large populations of grizzly bears, mountain goats, whales, seals, and eagles." },
-
             new Park { ParkId = 5, Name = "Catalina State Park", State = "Arizona", Type = "State Park", Description = "Preserves a diverse desert landscape at the foot of the Santa Catalina Mountains." },
-
             new Park { ParkId = 6, Name = "Coral Pink Sand Dunes", State = "Utah", Type = "State Park", Description = "Preserves the only dune field on the Colorado Plateau, with a unique color caused by iron oxides and minerals in the Navajo sandstone." },
-
             new Park { ParkId = 7, Name = "Angel Island State Park", State = "California", Type = "State Park", Description = "Interprets an island in San Francisco Bay whose history encompasses Coast Miwok prehistory, ranching, the 1910–1940 Angel Island Immigration Station, and long military use." },
-
             new Park { ParkId = 8, Name = "Catharine Valley Trail", State = "New York", Type = "State Park", Description = "Encompasses a planned 12-mile (19 km) recreation trail that follows abandoned railroad grades and canal towpaths between Watkins Glen and Horseheads." },
-
             new Park { ParkId = 9, Name = "Acadia", State = "Maine", Type = "National Park", Description = "Covering most of Mount Desert Island and other coastal islands, Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes." },
-
             new Park { ParkId = 10, Name = "Biscayne", State = "Florida", Type = "National Park", Description = "Located in Biscayne Bay, this park at the north end of the Florida Keys has four interrelated marine ecosystems: mangrove forest, the Bay, the Keys, and coral reefs." }
         );
     }
